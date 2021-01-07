67fa21067c178e2a4fb1e2fa0514eceaa788339e
Mousa Zeid Baker <mzeb@moodagent.com>
2021-01-07 02:06:12 +0100

    Fix demo gif links in README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aws-lambda-typing",
-    version="1.0.0",
+    version="1.0.1",
     description="A package that provides type hints for AWS Lambda event, context and response objects",
     long_description=long_description,
     long_description_content_type="text/markdown",
