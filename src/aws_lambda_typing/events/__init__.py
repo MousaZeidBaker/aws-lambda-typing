44786a2bca42bdd18710ec98cd3839828cc362ea
Julio Delgado Jr <delgjul@amazon.com>
2021-10-26 21:57:17 -0400

    Update __init__.py

--- src/aws_lambda_typing/events/__init__.py
+++ src/aws_lambda_typing/events/__init__.py
@@ -1,6 +1,7 @@
 from .api_gateway_proxy import APIGatewayProxyEventV1, APIGatewayProxyEventV2
 from .cloud_watch_events import CloudWatchEventsMessageEvent
 from .cloud_watch_logs import CloudWatchLogsEvent
+from .cloudformation import CloudFormationEvent
 from .code_commit import CodeCommitMessageEvent
 from .code_pipeline import CodePipelineEvent
 from .config import ConfigEvent
@@ -13,3 +14,23 @@ from .s3_batch import S3BatchEvent
 from .ses import SESEvent
 from .sns import SNSEvent
 from .sqs import SQSEvent
+
+__all__ = [
+    "APIGatewayProxyEventV1",
+    "APIGatewayProxyEventV2",
+    "CloudFormationEvent",
+    "CloudWatchEventsMessageEvent",
+    "CloudWatchLogsEvent",
+    "CodeCommitMessageEvent",
+    "CodePipelineEvent",
+    "ConfigEvent",
+    "DynamoDBStreamEvent",
+    "KinesisFirehoseEvent",
+    "KinesisStreamEvent",
+    "MQEvent",
+    "S3Event",
+    "S3BatchEvent",
+    "SESEvent",
+    "SNSEvent",
+    "SQSEvent",
+]
