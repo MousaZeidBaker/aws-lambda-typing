9aa439d32967ae35567ca42d7ac6f66837786ffa
Julio Delgado Jr <delgjul@amazon.com>
2021-10-26 21:58:17 -0400

    Create cloudformation.py

---
+++ src/aws_lambda_typing/events/cloudformation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Any, Dict, Optional, TypedDict
+else:
+    from typing import Any, Dict, Optional
+
+    from typing_extensions import TypedDict
+
+class CloudFormationEvent(TypedDict):
+    """
+    CloudFormationEvent https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-requesttypes.html
+    Attributes:
+    ----------
+    RequestType: str
+    RequestId: str
+    ResponseURL: str
+    ResourceType: bool
+    LogicalResourceId: str
+    StackId: str
+    ResourceProperties: Dict[str, Any]
+    PhysicalResourceId: Optional[str] Only in Update/Delete events
+    OldResourceProperties: Optional[Dict[str, Any]] Only in Update events
+    """
+    RequestType: str
+    RequestId: str
+    ResponseURL: str
+    ResourceType: str
+    LogicalResourceId: str
+    StackId: str
+    ResourceProperties: Optional[Dict[str, Any]]
+    PhysicalResourceId: str
+    OldResourceProperties: Dict[str, Any]
